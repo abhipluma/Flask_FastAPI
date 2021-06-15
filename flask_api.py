858765047ee3942348923b483170d73efb6e5d30
shraddha-pluma <shraddha.yadav@pluma.co>
2021-06-15 18:47:27 +0530

    updated sql raw query

--- flask_api.py
+++ flask_api.py
@@ -189,22 +189,29 @@ def client(skip=0, limit=100):
     data = []
     if 'sql' in request.args:
         sql = text('''
-                   select "id", CONCAT("firstName", "lastName") as "client_name", "email" as "client_email",
-                   "your_role" as "role", "zipCode" as "zip_code", "highestEducation" as "education", 
-                   (select "display_name" AS "group" FROM client_onboarding_clientgroup as CG where CG.id=COC.group_id ), 
-                   (select "language" FROM coach_onboarding_language as CL where CL.id=COC.preferred_language_id ),
-                   (select "country" FROM coach_onboarding_country as CC where CC.id=COC.country_id ),
-                   (select "option" AS "number_of_people_reporting" FROM client_onboarding_numberofpeoplereporting as CPR where CPR.id=COC.number_of_people_reporting_id),
-                   (select CONCAT(EDHP.hr_first_name, EDHP.hr_last_name) as hr_partner from enterprice_dashboard_hrpartnermapping as EDHP where EDHP.client_id=COC.id limit 1),
-                   (select count(*) as manager_call_count from chat_managerthreewaycallsession as CMTCS where CMTCS.client_id=COC.id),
-                   (select notes from enterprice_dashboard_notes as EDN where EDN.client_id=COC.id limit 1),
-                   (select CONCAT("firstName", "lastName") AS "coach_name" FROM coach_onboarding_coach as coach where coach.id="assignedCoach_id")
-                   from client_onboarding_client as COC WHERE COC.is_test_account = false  
-                   ''')
+                           select "id", CONCAT("firstName", "lastName") as "client_name", "email" as "client_email",
+                           "your_role" as "role", "zipCode" as "zip_code", "highestEducation" as "education", 
+                           (select "display_name" AS "group" FROM client_onboarding_clientgroup as CG where CG.id=COC.group_id ), 
+                           (select "language" FROM coach_onboarding_language as CL where CL.id=COC.preferred_language_id ),
+                           (select "country" FROM coach_onboarding_country as CC where CC.id=COC.country_id ),
+                           (select "option" AS "number_of_people_reporting" FROM client_onboarding_numberofpeoplereporting as CPR where CPR.id=COC.number_of_people_reporting_id),
+                           (select CONCAT(EDHP.hr_first_name, EDHP.hr_last_name) as hr_partner from enterprice_dashboard_hrpartnermapping as EDHP where EDHP.client_id=COC.id limit 1),
+                           (select count(*) as manager_call_count from chat_managerthreewaycallsession as CMTCS where CMTCS.client_id=COC.id),
+                           (select notes from enterprice_dashboard_notes as EDN where EDN.client_id=COC.id limit 1),
+                           (select CONCAT("firstName", "lastName") AS "coach_name" FROM coach_onboarding_coach as coach where coach.id="assignedCoach_id"),
+                           (select "option" AS "number_of_people_reporting" FROM client_onboarding_numberofpeoplereporting as CPR where CPR.id=COC.number_of_people_reporting_id ),
+                           (select "data" FROM client_onboarding_clientextrainfo as CEI where CEI.client_id=COC.id),
+                           (select count(*) AS "completed_360_num" FROM exercise_useranswermapper as EUAM where EUAM.user_id=COC.user_id and EUAM.is_reassessment = False and EUAM.answered_by_id is not null and EUAM.answered =True )
+                           from client_onboarding_client as COC WHERE COC.is_test_account = false  
+                           ''')
+        # '(select "related_as"  FROM exercise_peopleansweringexercise as EPAE where EPAE.id=EUAM.answered_by_id ), '
+        # (select COALESCE("end_date", COC.coach_payment_start_date) AS # "engagement_end_date" FROM client_onboarding_engagementtracker as COET where COET.client_id = COC.id AND COET.coach_id = "assignedCoach_id" and COET.end_date is not null limit 1),
+        # (select COALESCE("extended_on") AS "engagement_extend_date" FROM client_onboarding_engagementextendinfo as COEEI where COEEI.client_id = COC.id AND COEEI.coach_id = "assignedCoach_id" limit 1)
         sql_df = pandas.read_sql(
             sql,
-            con=db.engine
+            con=db.engine,
         )
+        # sql_df = sql_df.dropna()
         data = sql_df.to_dict('records')
     else:
         db_clients = db.session.query(Client).all()
